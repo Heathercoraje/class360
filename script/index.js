32a1e56e8ae154b26ea6badd72ff659fea7c2b86
heathercoraje <heathercoraje@gmail.com>
2018-04-09 13:12:26 +0900

    refactor stylesheet

--- script/index.js
+++ script/index.js
@@ -1,228 +1,228 @@
-//animation for contact page
-	function Constellation () {
-		var archorPoint = document.getElementById('canvas');
+// canvas
+function Constellation() {
+	var archorPoint = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+	var defaults = {
+		star: {
+			color: 'rgba(255, 255, 255, 0.8)',
+			width: 3,
+			randomWidth: true
+		},
+		line: {
+			color: 'rgba(255, 255, 255, 0.7)',
+			width: 0.7
+		},
+		position: {
+			x: 0,
+			y: 0
+		},
+		width: window.innerWidth,
+		height: window.innerHeight,
+		velocity: 0.2,
+		length: 300,
+		distance: 80,
+		radius: 200,
+		stars: []
+	};
 
-		var context = canvas.getContext('2d');
-		var defaults = {
-				star: {
-					color: 'rgba(255, 255, 255, .9)',
-					width: 5,
-					randomWidth: true
-				},
-				line: {
-					color: 'rgba(255, 255, 255, .5)',
-					width: 1
-				},
-				position: {
-					x: 0,
-					y: 0
-				},
-				width: window.innerWidth,
-				height: window.innerHeight,
-				velocity: 0.1,
-				length: 300,
-				distance: 80,
-				radius: 200,
-				stars: []
-			};
-		var config = Object.assign({}, defaults);
+	var config = Object.assign({}, defaults);
 
-		function Star () {
-			this.x = Math.random() * canvas.width;
-			this.y = Math.random() * canvas.height;
+	function Star() {
+		this.x = Math.random() * canvas.width;
+		this.y = Math.random() * canvas.height;
 
-			this.vx = (config.velocity - (Math.random() * 0.1));
-			this.vy = (config.velocity - (Math.random() * 0.1));
+		this.vx = config.velocity - Math.random() * 0.1;
+		this.vy = config.velocity - Math.random() * 0.1;
 
-			this.radius = config.star.randomWidth ? (Math.random() * config.star.width) : config.star.width;
-		}
+		this.radius = config.star.randomWidth
+			? Math.random() * config.star.width
+			: config.star.width;
+	}
 
-		Star.prototype = {
-			create: function(){
-				context.beginPath();
-				context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-				context.fill();
-			},
+	Star.prototype = {
+		create: function() {
+			context.beginPath();
+			context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+			context.fill();
+		},
 
-			animate: function(){
-				var i;
-				for (i = 0; i < config.length; i++) {
+		animate: function() {
+			var i;
+			for (i = 0; i < config.length; i++) {
+				var star = config.stars[i];
 
-					var star = config.stars[i];
-
-					if (star.y < 0 || star.y > canvas.height) {
-						star.vx = star.vx;
-						star.vy = - star.vy;
-					} else if (star.x < 0 || star.x > canvas.width) {
-						star.vx = - star.vx;
-						star.vy = star.vy;
-					}
-
-					star.x += star.vx;
-					star.y += star.vy;
+				if (star.y < 0 || star.y > canvas.height) {
+					star.vx = star.vx;
+					star.vy = -star.vy;
+				} else if (star.x < 0 || star.x > canvas.width) {
+					star.vx = -star.vx;
+					star.vy = star.vy;
 				}
-			},
+				star.x += star.vx;
+				star.y += star.vy;
+			}
+		},
 
-			line: function(){
-				var length = config.length,
-					iStar,
-					jStar,
-					i,
-					j;
+		line: function() {
+			var length = config.length,
+				iStar,
+				jStar,
+				i,
+				j;
 
-				for (i = 0; i < length; i++) {
-					for (j = 0; j < length; j++) {
-						iStar = config.stars[i];
-						jStar = config.stars[j];
-						//for distance between stars
+			for (i = 0; i < length; i++) {
+				for (j = 0; j < length; j++) {
+					iStar = config.stars[i];
+					jStar = config.stars[j];
+					//for distance between stars
+					if (
+						iStar.x - jStar.x < config.distance &&
+						iStar.y - jStar.y < config.distance &&
+						iStar.x - jStar.x > -config.distance &&
+						iStar.y - jStar.y > -config.distance
+					) {
+						//for mouse interaction
 						if (
-							(iStar.x - jStar.x) < config.distance &&
-							(iStar.y - jStar.y) < config.distance &&
-							(iStar.x - jStar.x) > - config.distance &&
-							(iStar.y - jStar.y) > - config.distance
+							iStar.x - config.position.x < config.radius &&
+							iStar.y - config.position.y < config.radius &&
+							iStar.x - config.position.x > -config.radius &&
+							iStar.y - config.position.y > -config.radius
 						) {
-							//for mouse interaction
-							if (
-								(iStar.x - config.position.x) < config.radius &&
-								(iStar.y - config.position.y) < config.radius &&
-								(iStar.x - config.position.x) > - config.radius &&
-								(iStar.y - config.position.y) > - config.radius
-							) {
-								context.beginPath();
-								context.moveTo(iStar.x, iStar.y);
-								context.lineTo(jStar.x, jStar.y);
-								context.stroke();
-								context.closePath();
-							}
+							context.beginPath();
+							context.moveTo(iStar.x, iStar.y);
+							context.lineTo(jStar.x, jStar.y);
+							context.stroke();
+							context.closePath();
 						}
 					}
 				}
 			}
+		}
+	};
+
+	this.createStars = function() {
+		var length = config.length,
+			star,
+			i;
+
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		for (i = 0; i < length; i++) {
+			config.stars.push(new Star());
+			star = config.stars[i];
+
+			star.create();
+		}
+
+		star.line();
+		star.animate();
+	};
+
+	this.setCanvas = function() {
+		canvas.width = config.width;
+		canvas.height = config.height;
+	};
+
+	this.setContext = function() {
+		context.fillStyle = config.star.color;
+		context.strokeStyle = config.line.color;
+		context.lineWidth = config.line.width;
+	};
+
+	this.setInitialPosition = function() {
+		config.position = {
+			x: canvas.width * 0.5,
+			y: canvas.height * 0.5
 		};
+	};
 
-		this.createStars = function () {
-			var length = config.length,
-				star,
-				i;
+	this.loop = function(callback) {
+		callback();
 
-			context.clearRect(0, 0, canvas.width, canvas.height);
-
-			for (i = 0; i < length; i++) {
-				config.stars.push(new Star());
-				star = config.stars[i];
-
-				star.create();
-			}
-
-			star.line();
-			star.animate();
-		};
-
-		this.setCanvas = function () {
-			canvas.width = config.width;
-			canvas.height = config.height;
-		};
-
-		this.setContext = function () {
-			context.fillStyle = config.star.color;
-			context.strokeStyle = config.line.color;
-			context.lineWidth = config.line.width;
-		};
-
-		this.setInitialPosition = function () {
-				config.position = {
-					x: canvas.width * 0.5,
-					y: canvas.height * 0.5
-				};
-		};
-
-		this.loop = function (callback) {
-			callback();
-
-			window.requestAnimationFrame(function () {
+		window.requestAnimationFrame(
+			function() {
 				this.loop(callback);
-			}.bind(this));
-		};
+			}.bind(this)
+		);
+	};
 
-		this.mouse = function () {
-			archorPoint.addEventListener('mousemove', function(e){
-				config.position.x = e.clientX;
-				config.position.y = e.clientY;
-			});
-		};
+	this.mouse = function() {
+		archorPoint.addEventListener('mousemove', function(e) {
+			config.position.x = e.clientX;
+			config.position.y = e.clientY;
+		});
+	};
 
-		this.init = function () {
-			this.setCanvas();
-			this.setContext();
-			this.setInitialPosition();
-			this.loop(this.createStars);
-			this.mouse();
-		};
-	}
+	//init!
+	this.init = function() {
+		this.setCanvas();
+		this.setContext();
+		this.setInitialPosition();
+		this.loop(this.createStars);
+		this.mouse();
+	};
+}
 
-  var c = new Constellation();
-			c.init();
+var c = new Constellation();
+c.init();
 
-//listeners
-function listener(buttonClass, targetClass, event, state, cb){
+//listeners for button manipulation
+function listener(buttonClass, targetClass, event, state, cb) {
 	var addElementListener = document.getElementsByClassName(buttonClass)[0];
 	var target = document.getElementsByClassName(targetClass)[0];
-	addElementListener.addEventListener(event,
-	e => {
-		if(state)target.style.display = state;
-		if(cb)cb(target, e);
+	addElementListener.addEventListener(event, e => {
+		if (state) target.style.display = state;
+		if (cb) cb(target, e);
 	});
 }
 
-function stopVideo(target){
+function stopVideo(target) {
 	var getVideoPlayer = target.querySelector('iframe');
 	var temp = getVideoPlayer.src;
 	getVideoPlayer.src = temp;
 }
 
-// function formAction(placeholder, e){
-// 	e.preventDefault();
-// 	// e.target.elements[0].value,e.target.elements[1].value,e.target.elements[2].value,e.target.elements[3].value
-// 	e.target.reset();
-// }
-
-listener('button__video', 'container__video', 'click', 'block');
-listener('button__instruct', 'container__video__instr', 'click', 'block');
-listener('container__video__instr', 'container__video__instr', 'click', 'none', stopVideo);
-listener('container__video', 'container__video', 'click', 'none', stopVideo);
-
 // listener('form', null, 'submit', null, formAction);
+// video on #landing
+listener('button-video', 'container-video', 'click', 'block');
+listener('container-video', 'container-video', 'click', 'none', stopVideo);
 
-var nav = document.getElementsByTagName("nav")[0];
-var landing = document.getElementById("landing");
-var image = document.getElementById("instruct-pic");
-var contact = document.getElementById("contact");
-nav.style.color = "white";
+// video on #instruction
+listener('button__instruct', 'container__video__instr', 'click', 'block');
+listener(
+	'container__video__instr',
+	'container__video__instr',
+	'click',
+	'none',
+	stopVideo
+);
 
-window.addEventListener('scroll', e =>{
+// for nav bar color (transparent) effect for first and last section
+window.addEventListener('scroll', e => {
+	var nav = document.getElementsByTagName('nav')[0];
+	var landing = document.getElementById('landing');
+	var contact = document.getElementById('contact');
+	nav.style.color = 'white'; // this has to move to style sheet for
 	var navTop = nav.getBoundingClientRect().top;
 	var landingTop = landing.getBoundingClientRect().top;
-	// var imageTop = image.getBoundingClientRect().top;
 	var contactTop = contact.getBoundingClientRect().top;
 	var navBottom = nav.getBoundingClientRect().bottom;
 	var landingBottom = landing.getBoundingClientRect().bottom;
-	// var imageBottom = image.getBoundingClientRect().bottom;
 	var contactBottom = contact.getBoundingClientRect().bottom;
-
-
-	console.log('navBottom', navBottom, 'navTop', navTop);
-	console.log('contactBottom', contactBottom, 'contactTop', contactTop);
-	if(((landingTop <= navTop) && (navTop <= landingBottom)) &&
-		 ((landingTop <= navBottom) && (navBottom <= landingBottom)))
-			 nav.style.backgroundColor = 'transparent';
-	else if(((contactTop <= navTop) && (navTop <= contactBottom)) &&
-		 ((contactTop <= navBottom) && (navBottom <= contactBottom))){
-			 console.log('inside contact');
-			 nav.style.backgroundColor = 'transparent';
-	}
-	else {
+	if (
+		landingTop <= navTop &&
+		navTop <= landingBottom &&
+		(landingTop <= navBottom && navBottom <= landingBottom)
+	)
+		nav.style.backgroundColor = 'transparent';
+	else if (
+		contactTop <= navTop &&
+		navTop <= contactBottom &&
+		(contactTop <= navBottom && navBottom <= contactBottom)
+	) {
+		console.log('inside contact');
+		nav.style.backgroundColor = 'transparent';
+	} else {
 		nav.style.backgroundColor = '#888888';
 	}
-}
-  // console.log('nav',nav.getBoundingClientRect().bottom, 'landing',landing.getBoundingClientRect().bottom)
-);
+});
